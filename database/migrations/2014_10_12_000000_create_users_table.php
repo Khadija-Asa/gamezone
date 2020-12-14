e230f9001165a2722e12ad88b996c557e57740b3
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-14 10:52:23 +0100

    made user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nickname')->unique;
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('city');
+            $table->string('avatar');
+            $table->bigInteger('exp');
+            $table->boolean('admin');
             $table->rememberToken();
             $table->timestamps();
         });
