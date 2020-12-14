8a5cc1cccd3338058b9a885176509aab3f9f0d1b
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-14 12:34:56 +0100

    actions for user are now secured (guests/user/admin)

--- resources/views/User/edit.blade.php
+++ resources/views/User/edit.blade.php
@@ -2,34 +2,47 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-8">
-			<form action="{{ route('User.update', ['User'=>$user->id]) }}" method="POST" >
-				@csrf
-				@method('PUT')
-				<div class="form-group">
-					<label for="nickname">Nom d'affichage</label>
-					<input value=" {{ $user->nickname }} " name="nickname" id="nickname" type="text" tabindex="1" required autofocus>
-				</div>
-				<div class="form-group">
-					<label for="first_name">Prénom</label>
-					<input value=" {{ $user->first_name }} " name="first_name" id="first_name" type="text" tabindex="1" required autofocus>
-				</div>
-				<div class="form-group">
-					<label for="last_name">Nom</label>
-					<input value=" {{ $user->last_name }} " name="last_name" id="last_name" type="text" tabindex="1" required autofocus>
-				</div>
-				<div class="form-group">
-					<label for="city">Ville</label>
-					<input value=" {{ $user->city }} " name="city" id="city" type="text" tabindex="1" required autofocus>
-				</div>
-				<div class="form-group">
-					<label for="email">Email</label>
-					<input value=" {{ $user->email }} " name="email" id="email" type="text" tabindex="1" required autofocus>
-				</div>
-				<button class="btn btn-primary" name="submit" type="submit" id="contact-submit">Modifier Produit</button>
-			</form>
-		</div>
-	</div>
+  @guest
+    <p>Vous n'avez pas la permission d'accéder à cette page</p>
+  @else
+    @if (Auth::user()->admin === 1 OR Auth::user()->id === $user->id)
+    <div class="row">
+      <div class="col-8">
+        <form action="{{ route('User.update', ['User'=>$user->id]) }}" method="POST" >
+          @csrf
+          @method('PUT')
+          <div class="form-group">
+            <label for="nickname">Nom d'affichage</label>
+            <input value=" {{ $user->nickname }} " name="nickname" id="nickname" type="text" tabindex="1" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="first_name">Prénom</label>
+            <input value=" {{ $user->first_name }} " name="first_name" id="first_name" type="text" tabindex="1" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="last_name">Nom</label>
+            <input value=" {{ $user->last_name }} " name="last_name" id="last_name" type="text" tabindex="1" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="city">Ville</label>
+            <input value=" {{ $user->city }} " name="city" id="city" type="text" tabindex="1" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input value=" {{ $user->email }} " name="email" id="email" type="text" tabindex="1" required autofocus>
+          </div>
+          <button class="btn btn-primary" name="submit" type="submit" id="contact-submit">Modifier mes informations</button>
+        </form>
+        <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
+          @csrf
+          @method('DELETE')
+          <button class="btn btn-danger" type="submit">
+            <span class="fa fa-trash">Supprimer le compte</span>
+          </button>
+        </form>
+      </div>
+    </div>
+    @endif
+  @endguest
 </div>
 @endsection
--- resources/views/User/show.blade.php
+++ resources/views/User/show.blade.php
@@ -4,35 +4,51 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-8">
-			<ul class="list-group">
-				<li class="list-group-item active">
-					Nom d'affichage: {{ $user->nickname }}
-				</li>
-				<li class="list-group-item active">
-					Email: {{ $user->email }}
-				</li>
-				<li class="list-group-item active">
-					Prénom: {{ $user->first_name }}
-				</li>
-				<li class="list-group-item active">
-					Nom: {{ $user->last_name }}
-				</li>
-				<li class="list-group-item active">
-					Ville: {{ $user->city }}
-				</li>
-				<li class="list-group-item active">
-					Avatar: {{ $user->avatar }}
-				</li>
-				<li class="list-group-item active">
-					Expérience: {{ $user->exp }}
-				</li>
-				<li class="list-group-item active">
-					Administrateur: {{ $user->admin }}
-				</li>
-			</ul>
-		</div>
-	</div>
+  @guest <!-- Si l'utilisateur est un guest -->
+  <p>Veuillez vous connecter pour accéder à vos informations</p>
+  @else
+    @if (Auth::user()->admin === 1 OR Auth::user()->id === $user->id) <!-- Si l'utilisateur est admin OU si l'id demandée est l'id de l'utilsiateur connecté -->
+    <div class="row">
+      <div class="col-8">
+        <ul class="list-group">
+          <li class="list-group-item active">
+            Nom d'affichage: {{ $user->nickname }}
+          </li>
+          <li class="list-group-item active">
+            Email: {{ $user->email }}
+          </li>
+          <li class="list-group-item active">
+            Prénom: {{ $user->first_name }}
+          </li>
+          <li class="list-group-item active">
+            Nom: {{ $user->last_name }}
+          </li>
+          <li class="list-group-item active">
+            Ville: {{ $user->city }}
+          </li>
+          <li class="list-group-item active">
+            Avatar: {{ $user->avatar }}
+          </li>
+          <li class="list-group-item active">
+            Expérience: {{ $user->exp }}
+          </li>
+          <li class="list-group-item active">
+            Administrateur: {{ $user->admin }}
+          </li>
+        </ul>
+        <a href="{{ route('User.edit', ['User' => $user->id]) }}" class="btn btn-primary">
+          <span class="fa fa-edit"> Modifier</span>
+        </a>
+        <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
+          @csrf
+          @method('DELETE')
+          <button class="btn btn-danger" type="submit">
+            <span class="fa fa-trash">Supprimer le compte</span>
+          </button>
+        </form>
+      </div>
+    </div>
+    @endif
+  @endguest
 </div>
 @endsection
