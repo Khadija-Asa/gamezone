69cca49a416d9fe19279523e870862dfbe354983
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-14 10:54:25 +0100

    made user model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nickname', 'password', 'email', 'first_name', 'last_name', 'city', 'avatar', 'exp', 'admin'
     ];
 
     /**
