bb784b687ee264c3b7607c48ad18566ce8c3f134
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-14 12:16:33 +0100

    added delete for user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -86,6 +86,9 @@ class UsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $user = User::find($id);
+      $user->delete();
+
+      return redirect()->route('User.index');
     }
 }
