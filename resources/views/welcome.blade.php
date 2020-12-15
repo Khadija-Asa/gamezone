c2a60808ca26a06eaeedcb1e2daee2124ecda7f9
Libert josic <libert.josic@gmail.com>
2020-12-15 16:12:59 +0100

    feat(index):content finish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,75 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+<div class="index">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <section class="header-bottom">
+        <h1>GAME ZONE</h1>
+        <p>Le premier parc d’attraction <br>
+        au monde entièrement dédié à <br>
+        l’univers des jeux-vidéos.</p>
+    </section>
 
-            .full-height {
-                height: 100vh;
-            }
+    <section class="presentation">
+        <div class="top">
+            <h2>Présentation</h2>
+            <p><span>GEEK CYBERCENTER</span> est une société spécialisé dans le <span>jeux vidéos</span>. Elle propose différents moyens de s’amuser,
+            de la réalité virtuelle au Kart sous une piste visuelle. Imaginez les <span>sensations inédites</span> que vont vous procurer ces attractions hors du commun ! 
+            Ces parcs propose une nouvelle forme de divertissement innovant alliant <span>monde réel et réalité augmentée</span>.
+            Geek Cybercenter est un groupe possédant plusieurs parcs d’attractions dans le monde comme : </p>
+            <ul>
+                <li>BATTLE KART VR</li>
+                <li>FORNITE ADVENTURE</li>
+                <li>PUBG SURVIVOR et bien d'autres.</li>
+            </ul>
+            <p>Gamers du monde entier : À vous de jouer !</p>
+        </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <div class="center">
+            <div>
+                <p>9</p>
+                <p>attractions</p>
+            </div>
+            <div>
+                <p>5</p>
+                <p>restaurants</p>
+            </div>
+            <div>
+                <p>500k</p>
+                <p>visiteurs</p>
+            </div>
+            <div>
+                <p>9000</p>
+                <p>hectares</p>
             </div>
         </div>
-    </body>
-</html>
+        <div class="bottom"></div>
+    </section>
+
+    <section class="main-attraction">
+        <h2>LES ATTRACTIONS</h2>
+        <div class="top">
+            <p>Venez vous <span>amuser</span> dans l’une de nos attractions !<br>
+                Massacrez des zombies, provoquez un déluge de la justice ou pilotez des karts : chacunes de nos attractions 
+                possèdent ses propres caractéristiques. <span>Testez les toutes !</span></p>
+            <a href="">En savoir plus</a>
+        </div>
+        <div class="center">
+            <img src="../images/gamecente.png" alt="logo-gamecenter">
+            <img src="../images/awsomeheros.png" alt="logo-awsomeheros">
+            <img src="../images/championsleaguesurvivor.png" alt="logo-champions-leagie-survivor">
+            <img src="../images/heroesteam.png" alt="logo-heroesteam">
+        </div>
+        <div class="bottom"></div>
+    </section>
+    <section class="news">
+        <h2>NEWS</h2>
+
+    </section>
+
+    
+    
+</div>
+
+@endsection
