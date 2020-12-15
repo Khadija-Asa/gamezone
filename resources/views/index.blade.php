473964c893af2efc3f6237d974cce237b6b6fdef
Libert josic <libert.josic@gmail.com>
2020-12-15 10:45:33 +0100

    feat(index):first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Tous les utilisateurs')
+
+@section('content')
+<div class="container">
+  <div class="index">
+  
+  </div>
+
+
+
+</div>
+@endsection
