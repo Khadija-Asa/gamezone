fbea464cb2a037716d7ab38f8ce43a4f9a973f65
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-15 09:54:38 +0100

    added migration

--- app/Attraction.php
+++ app/Attraction.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attraction extends Model
 {
-    //
+    protected $fillable = ([
+      'name', 'logo_url', 'bg_image_url', 'description', 'impportant_informations', 'min_height', 'exp_given'
+    ]);
 }
