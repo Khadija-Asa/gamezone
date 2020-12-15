cda9d1bdacf0f5f072d8b98b643f784e238bc9ab
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-15 09:52:31 +0100

    added migration

---
+++ database/migrations/2020_12_15_084636_create_attractions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttractionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attractions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('logo_url');
+            $table->string('bg_image_url')->nullable();
+            $table->text('description')->nullable();
+            $table->text('important_informations')->nullable();
+            $table->integer('min_height');
+            $table->integer('exp_given');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attractions');
+    }
+}
