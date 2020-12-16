42e39a0079201dffc798962fb3fd40a16d6177de
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-16 17:23:08 +0100

    added style for pricelist

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,10 @@
     <link href="{{ asset('css/map.css') }}" rel="stylesheet">
     <link href="{{ asset('css/contact.css') }}" rel="stylesheet">
     <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    
 		<link href="{{ asset('css/header.css') }}" rel="stylesheet">
 
+    <link href="{{ asset('css/pricelist.css') }}" rel="stylesheet">
 </head>
 
 <body>
