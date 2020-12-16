c0ee470cba04cf80d7eb23199bcae7a46f9e2479
Bruno Bastin <bruno59490@hotmail.fr>
2020-12-16 13:08:25 +0100

    feat(contact)add style including background, second background, button and padding/margin, media screen 800px 992px and 1100px, adding picture background

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 <div class="container-contact">
-  <div class="row">
-    <div class="col-8">
+  <div class="row-contact">
+  <p class="titlecontact">Pour toutes demandes vous pouvez nous contacter :</p>
+    <div class="col-contact">
       @if (isset($success))
         <p>Message envoyé !</p>
       @endif
@@ -13,22 +14,26 @@
         <div class="contact-part-one">
         <div class="form-group-contact">
           <label for="name">Nom</label>
-          <input name="name" id="name" type="text" tabindex="1" required autofocus>
+          <input name="name" id="name" type="text" placeholder="Nom" tabindex="1" required autofocus>
         </div>
         <div class="form-group-contact">
           <label for="mail">Adresse mail</label>
-          <input name="mail" id="mail" type="text" tabindex="1" required autofocus>
+          <input name="mail" id="mail" type="text" placeholder="Adresse mail" tabindex="1" required autofocus>
         </div>
         </div>
         <div class="contact-part-two">
         <div class="form-group-contact">
           <label for="message">Message</label>
-          <textarea name="message" id="message" type="text" tabindex="1" rows="7" cols="33" required autofocus></textarea>
+          <textarea name="message" id="message" type="text" placeholder="Message" tabindex="1" rows="7" cols="33" required autofocus></textarea>
         </div>
         </div>
+        </div>
+        <div class="checkboxcontact">
+        <input type="checkbox">
+        <p class="rgpdcontact">J'accepte que les informations saisies soient utilisées exclusivement dans le cadre de ma demande.</p>
         </div>
         <div class="contactbutton">
-        <button class="btn btn-primary" name="submit" type="submit" id="contact-submit">Envoyer le message</button>
+        <button class="buttoncontact" name="submit" type="submit" id="contact-submit">Envoyer</button>
         </div>
       </form>
     </div>
