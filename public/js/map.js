2f6f77d514ffb5fab6ae66bf58fef91053cd4d46
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-16 01:14:03 +0100

    placed all markers accross the map; added checkboxes to toggle markers

--- public/js/map.js
+++ public/js/map.js
@@ -1,1 +1,43 @@
-console.log('u')
+document.addEventListener('DOMContentLoaded', function() { //On check quand le HTML sera complètement chargé
+
+    document.querySelector('#servicesCheckbox').addEventListener('click', () => { //On met un event Listener click sur la checkbox des services
+        document.querySelectorAll('#services').forEach( e => { //On boucle tous les marqueurs qui ont l'id services
+            if (e.style.display === "none") { //Si leur style display est à "none"
+                e.style.display = "block"; //On les affiche en mettant un display: block
+            } else {
+                e.style.display = "none";
+            }
+        });
+    });
+
+    document.querySelector('#restaurantsCheckbox').addEventListener('click', () => { 
+        document.querySelectorAll('#restaurants').forEach( e => { 
+            if (e.style.display === "none") { 
+                e.style.display = "block"; 
+            } else {
+                e.style.display = "none";
+            }
+        });
+    });
+
+    document.querySelector('#shopsCheckbox').addEventListener('click', () => { 
+        document.querySelectorAll('#shops').forEach( e => { 
+            if (e.style.display === "none") { 
+                e.style.display = "block"; 
+            } else {
+                e.style.display = "none";
+            }
+        });
+    });
+
+    document.querySelector('#photosCheckbox').addEventListener('click', () => { 
+        document.querySelectorAll('#photos').forEach( e => { 
+            if (e.style.display === "none") { 
+                e.style.display = "block"; 
+            } else {
+                e.style.display = "none";
+            }
+        });
+    });
+
+});
