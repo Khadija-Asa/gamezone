a4ffb4b406b7262b76242fd7f37bf4e18ecd2ced
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-16 13:57:14 +0100

    added style for calendar

--- resources/views/Calendar/index.blade.php
+++ resources/views/Calendar/index.blade.php
@@ -3,41 +3,50 @@
 @section('title', 'Tous les utilisateurs')
 
 @section('content')
-<div class="container">
-  <table>
-    <tr>
-      @for ($j = 0; $j < $monthOffset; $j++) <!-- On ajoute autant de case que necessaire pour combler la première semaine -->
-      <td style="border: 1px solid black; padding: 5px;">
-      </td>
-      @endfor
-      @for ($i = 0; $i < $days; $i++)
-        @if ($i+1 == $firstTuesday) <!-- Si on est sur la case du premier mardi du mois -->
-          <td style="background-color: #C4C4C4; border: 1px solid black; padding: 5px;">
-            {{ date('l', $first_day_timestamp+86400*$i) }} {{ $i+1 }} {{ $month }}
-          </td>
-        @else
-          @if (date('l', $first_day_timestamp+86400*$i) === 'Monday') <!-- Si on arrive à un Lundi, on change de ligne -->
-            </tr>
-            <tr>
-          @endif
-          @if (date('l', $first_day_timestamp+86400*$i) === 'Friday' || date('l', $first_day_timestamp+86400*$i) === 'Saturday') <!-- Si le jour est Vendredi ou Samedi -->
-            <td style="background-color: #070B34; border: 1px solid black; padding: 5px;">
-              {{ date('l', $first_day_timestamp+86400*$i) }} {{ $i+1 }} {{ $month }}
+<div class="calendarContainer">
+  <div class="calendar">
+    <table>
+      <th>L</th>
+      <th>M</th>
+      <th>M</th>
+      <th>J</th>
+      <th>V</th>
+      <th>S</th>
+      <th>D</th>
+      <tr>
+        @for ($j = 0; $j < $monthOffset; $j++) <!-- On ajoute autant de case que necessaire pour combler la première semaine -->
+        <td>
+        </td>
+        @endfor
+        @for ($i = 0; $i < $days; $i++)
+          @if ($i+1 == $firstTuesday) <!-- Si on est sur la case du premier mardi du mois -->
+            <td class="closed">
+              {{ $i+1 }}
             </td>
           @else
-            @if ($i+1 == $lastSunday)<!-- Si on arrive au dernier dimanche du mois -->
-              <td style="background-color: #881F6A; border: 1px solid black; padding: 5px;">
-                {{ date('l', $first_day_timestamp+86400*$i) }} {{ $i+1 }} {{ $month }}
+            @if (date('l', $first_day_timestamp+86400*$i) === 'Monday') <!-- Si on arrive à un Lundi, on change de ligne -->
+              </tr>
+              <tr>
+            @endif
+            @if (date('l', $first_day_timestamp+86400*$i) === 'Friday' || date('l', $first_day_timestamp+86400*$i) === 'Saturday') <!-- Si le jour est Vendredi ou Samedi -->
+              <td class="byNight">
+                {{ $i+1 }}
               </td>
-            @else <!-- Un jour normal -->
-              <td style="background-color: #1B8900; border: 1px solid black; padding: 5px;">
-                {{ date('l', $first_day_timestamp+86400*$i) }} {{ $i+1 }} {{ $month }}
-              </td>
+            @else
+              @if ($i+1 == $lastSunday)<!-- Si on arrive au dernier dimanche du mois -->
+                <td class="special">
+                  {{ $i+1 }}
+                </td>
+              @else <!-- Un jour normal -->
+                <td class="ordinary">
+                  {{ $i+1 }}
+                </td>
+              @endif
             @endif
           @endif
-        @endif
-      @endfor
-    </tr>
-  </table>
+        @endfor
+      </tr>
+    </table>
+  </div>
 </div>
 @endsection
