93b5466747863f84d151c6cec1d442acda973e1e
Bruno Bastin <bruno59490@hotmail.fr>
2020-12-17 11:34:11 +0100

    feat(account)add structure html to show.blade, renaming class, add style to show.blade

--- resources/views/User/show.blade.php
+++ resources/views/User/show.blade.php
@@ -3,50 +3,57 @@
 @section('title', 'Details Produits')
 
 @section('content')
-<div class="container">
-  @if (Auth::user()->admin === 1 OR Auth::user()->id === $user->id) <!-- Si l'utilisateur est admin OU si l'id demandée est l'id de l'utilsiateur connecté -->
-  <div class="row">
-    <div class="col-8">
-      <ul class="list-group">
-        <li class="list-group-item active">
-          Nom d'affichage: {{ $user->nickname }}
-        </li>
-        <li class="list-group-item active">
-          Email: {{ $user->email }}
-        </li>
-        <li class="list-group-item active">
-          Prénom: {{ $user->first_name }}
-        </li>
-        <li class="list-group-item active">
-          Nom: {{ $user->last_name }}
-        </li>
-        <li class="list-group-item active">
-          Ville: {{ $user->city }}
-        </li>
-        <li class="list-group-item active">
-          Avatar: <img src="{{ asset($user->avatar) }}" style="max-width: 200px;">
-        </li>
-        <li class="list-group-item active">
-          Expérience: {{ $user->exp }}
-        </li>
-        @if (Auth::user()->admin === 1)
-          <li class="list-group-item active">
-            Administrateur: {{ $user->admin }}
-          </li>
-        @endif
-      </ul>
-      <a href="{{ route('User.edit', ['User' => $user->id]) }}" class="btn btn-primary">
-        <span class="fa fa-edit"> Modifier</span>
-      </a>
-      <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
-        @csrf
-        @method('DELETE')
-        <button class="btn btn-danger" type="submit">
-          <span class="fa fa-trash">Supprimer le compte</span>
-        </button>
-      </form>
-    </div>
-  </div>
-  @endif
+<section class="header-bottom">
+        <h1>GAME ZONE</h1>
+        <p>MON COMPTE</p>
+</section>
+<div class="container-account">
+      <div class="group-account">
+          <div class="accountleft">
+          <ul class="list-group-account-left">
+            <li class="list-group-item-left">
+              <img src="{{ asset($user->avatar) }}" style="max-width: 200px;">
+            </li>
+            <li class="list-group-item-left">
+              Expérience: {{ $user->exp }}
+            </li>
+              <li class="list-group-item-left">
+                Administrateur: {{ $user->admin }}
+              </li>
+              </ul>
+          </div>
+          <div class="accountright">
+          <ul class="list-group-account-right">
+            <li class="list-group-item-right">
+              Pseudo: {{ $user->nickname }}
+            </li>
+            <li class="list-group-item-right">
+              Email: {{ $user->email }}
+            </li>
+            <li class="list-group-item-right">
+              Prénom: {{ $user->first_name }}
+            </li>
+            <li class="list-group-item-right">
+              Nom: {{ $user->last_name }}
+            </li>
+            <li class="list-group-item-right">
+              Ville: {{ $user->city }}
+            </li>
+        </ul>
+        </div>
+        </div>
+        <div class="buttonaccount">
+          <a href="{{ route('User.edit', ['User' => $user->id]) }}" class="btn btn-primary">
+            <button class="editbuttonaccount"><span class="fa fa-edit"> Modifier</span>
+            </button>
+          </a>
+          <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
+            @csrf
+            @method('DELETE')
+            <button class="deletebuttonaccount" type="submit">
+              <span class="fa fa-trash">Supprimer le compte</span>
+            </button>
+          </form>
+        </div>
 </div>
 @endsection
