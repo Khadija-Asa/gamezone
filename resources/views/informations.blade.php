c44d9a91736b894c293b2a7a78381f05e51cd4bc
Libert josic <libert.josic@gmail.com>
2020-12-17 16:44:12 +0100

    feat(infos):finish

--- resources/views/informations.blade.php
+++ resources/views/informations.blade.php
@@ -2,4 +2,32 @@
 
 @section('content')
 
+<section class="header-bottom">
+  <h1>GAME ZONE</h1>
+  <p>INFORMATIONS</p>
+</section>
+
+
+<section class="informations">
+
+  <div class="paragraphe">
+    <p><span>GEEK CYBERCENTER</span> est une SAS au capital de 45 824 123 euros. Il s’agit d’un groupe possédant plusieurs parcs d’attractions dans le monde comme : BATTLE KART VR, FORTNITE ADVENTURE, PUBG SURVIVOR. Ouvert en septembre 2019 c’est le premier parc d’attraction au monde entièrement dédié à l’univers des jeux-vidéo.
+
+    <br> vous soyez, <span>petit</span> ou <span>grand, gamer confirmé</span> ou <span>débutant</span>, toutes les attractions vous assurent une journée riche en rires et en action, encadré par nos mesures de sécurité et sanitaire. Bienvenue dans ce nouveau monde !
+    </p>
+  </div>
+
+  <div class="image-information">
+    
+  </div>
+
+  <div class="paragraphe">
+    <p><span>NOTRE MISSION</span><br>
+Nous nous consacrons à la création des expériences ludiques les plus épiques qui aient jamais existé. Vous pourrez essayer des attractions uniques que vous ne trouverez nul part ailleurs et qui contribueront à faire augmenter votre jauge d’expérience, seul ou en équipe, devenez le numéro 1 !</p>
+  </div>
+
+</section>
+
+
+
 @endsection
