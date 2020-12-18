eb6e0f1f8a23fb20a34fad65103de26866611907
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-18 12:53:48 +0100

    added style for users list

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,42 +1,39 @@
 @extends('layouts.app')
 
-@section('title', 'Tous les utilisateurs')
-
 @section('content')
-<div class="container">
-  <div class="row">
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Utilisateur :</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($users as $user)
-            <tr>
-              <td>
-                {{ $user->nickname }}
-              </td>
-              <td>
-                <a href="{{ route('User.show', ['User' => $user->id]) }} " class="btn btn-success">
-                  <span class="fa fa-eye"> Détails</span>
-                </a>
-                <a href="{{ route('User.edit', ['User' => $user->id]) }}" class="btn btn-primary">
-                  <span class="fa fa-edit"> Modifier</span>
-                </a>
-                <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">
-                    <span class="fa fa-trash">Supprimer</span>
-                  </button>
-                </form>
-              </td>
-            </tr>
-        @endforeach
-      </tbody>
-    </table>
-  </div>
+<div class="containerHours">
+  <h2>Liste des utilisateurs</h2>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Utilisateur</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($users as $user)
+          <tr>
+            <td>
+              {{ $user->nickname }}
+            </td>
+            <td>
+              <a href="{{ route('User.show', ['User' => $user->id]) }} " class="btn btn-success">
+                <span class="fa fa-eye"> Détails</span>
+              </a>&nbsp;
+              <a href="{{ route('User.edit', ['User' => $user->id]) }}" class="btn btn-primary">
+                <span class="fa fa-edit"> Modifier</span>
+              </a>&nbsp;
+              <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
+                @csrf
+                @method('DELETE')
+                <button class="editbuttonaccount" type="submit">
+                  <span class="fa fa-trash">Supprimer</span>
+                </button>
+              </form>
+            </td>
+          </tr>
+      @endforeach
+    </tbody>
+  </table>
 </div>
 @endsection
