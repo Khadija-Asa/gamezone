de4980b9ef8471017c2706be178b02f49e779ad5
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-19 13:31:07 +0100

    added index for cart

--- app/Cart.php
+++ app/Cart.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Cart extends Model
 {
     protected $fillable = ([
         'status', 'user_id'
     ]);
+
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
 }
