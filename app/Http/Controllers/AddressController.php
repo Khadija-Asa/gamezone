db1b6eff084af72d1a8084f5905f9998b05e9e48
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-20 13:26:44 +0100

    fixed incorrect addresses shown

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -18,8 +18,8 @@ class AddressController extends Controller
      */
     public function index()
     {
-      $adresses = Address::with(['addresses'])->find(Auth::id());
-      return view('Address.index', ['addresses' => $adresses]);
+      $user = User::with(['getAddresses'])->find(Auth::id());
+      return view('Address.index', ['user' => $user]);
     }
 
     /**
--- resources/views/Address/index.blade.php
+++ resources/views/Address/index.blade.php
@@ -3,46 +3,40 @@
 @section('title', 'Tous les utilisateurs')
 
 @section('content')
-<div class="container">
-  @guest
-    <p>Vous n'avez pas la permission d'accéder à cette page</p>
-  @else
-    <div class="row">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Adresse :</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          @if (isset($addresses))
-            @foreach ($addresses->addresses as $address)
-                <tr>
-                  <td>
-                    {{ $address->address->address }}, {{ $address->address->zip_code }} {{ $address->address->city }} {{ $address->address->country }}
-                  </td>
-                  <td>
-                    <a href="{{ route('Address.edit', ['Address' => $address->address->id]) }}" class="btn btn-primary" title="éditer adresse">
-                      <span class="fa fa-edit"> Modifier</span>
-                    </a>
-                    <form action="{{ route('Address.destroy', ['Address' => $address->address->id]) }}" method="POST" style="display: contents">
-                      @csrf
-                      @method('DELETE')
-                      <button class="btn btn-danger" type="submit">
-                        <span class="fa fa-trash">Supprimer</span>
-                      </button>
-                    </form>
-                  </td>
-                </tr>
-            @endforeach
-          @endif
-        </tbody>
-      </table>
-      <a href="{{ route('Address.create') }}" class="btn btn-primary" title="lien vers créer adresse">
-        <span class="fa fa-edit">Ajouter une adresse</span>
-      </a>
-    </div>
-  @endguest
+<div class="containerHours">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Adresse</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      @if (isset($user))
+        @foreach ($user->getAddresses as $address)
+            <tr>
+              <td>
+                {{ $address->address->address }}, {{ $address->address->zip_code }} {{ $address->address->city }} {{ $address->address->country }}
+              </td>
+              <td>
+                <a href="{{ route('Address.edit', ['Address' => $address->address->id]) }}" class="btn btn-primary" title="éditer adresse">
+                  <span class="fa fa-edit"> Modifier</span>
+                </a>
+                <form action="{{ route('Address.destroy', ['Address' => $address->address->id]) }}" method="POST" style="display: contents">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">
+                    <span class="fa fa-trash">Supprimer</span>
+                  </button>
+                </form>
+              </td>
+            </tr>
+        @endforeach
+      @endif
+    </tbody>
+  </table>
+  <a href="{{ route('Address.create') }}" class="btn btn-primary" title="lien vers créer adresse">
+    <span class="fa fa-edit">Ajouter une adresse</span>
+  </a>
 </div>
 @endsection
