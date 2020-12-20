7824e8907ab11cff1330ed18507df287d775794c
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-20 13:33:40 +0100

    added style to index adresses

--- resources/views/Address/index.blade.php
+++ resources/views/Address/index.blade.php
@@ -19,13 +19,13 @@
                 {{ $address->address->address }}, {{ $address->address->zip_code }} {{ $address->address->city }} {{ $address->address->country }}
               </td>
               <td>
-                <a href="{{ route('Address.edit', ['Address' => $address->address->id]) }}" class="btn btn-primary" title="éditer adresse">
+                <a href="{{ route('Address.edit', ['Address' => $address->address->id]) }}" class="editbutton" title="éditer adresse" title="modifier l'adresse">
                   <span class="fa fa-edit"> Modifier</span>
                 </a>
                 <form action="{{ route('Address.destroy', ['Address' => $address->address->id]) }}" method="POST" style="display: contents">
                   @csrf
                   @method('DELETE')
-                  <button class="btn btn-danger" type="submit">
+                  <button class="editbutton" type="submit">
                     <span class="fa fa-trash">Supprimer</span>
                   </button>
                 </form>
@@ -35,7 +35,8 @@
       @endif
     </tbody>
   </table>
-  <a href="{{ route('Address.create') }}" class="btn btn-primary" title="lien vers créer adresse">
+  <br>
+  <a href="{{ route('Address.create') }}" class="editbutton" title="lien vers créer adresse">
     <span class="fa fa-edit">Ajouter une adresse</span>
   </a>
 </div>
