8c64d8d0f89648b9a978193fb755ae78b92452b3
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-20 13:43:05 +0100

    added style for edit address

--- resources/views/Address/edit.blade.php
+++ resources/views/Address/edit.blade.php
@@ -2,41 +2,41 @@
 
 @section('content')
 <div class="container">
-  @guest
-    <p>Vous n'avez pas la permission d'accéder à cette page</p>
-  @else
-    <div class="row">
-      <div class="col-8">
-        <form action="{{ route('Address.update', ['Address'=>$address->id]) }}" method="POST" >
-          @csrf
-          @method('PUT')
-          <div class="form-group">
-            <label for="address">Adresse</label>
-            <input value=" {{ $address->address }} " name="address" id="address" type="text" tabindex="1" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="zip_code">Cope postal</label>
-            <input value=" {{ $address->zip_code }} " name="zip_code" id="zip_code" type="text" tabindex="1" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="city">Ville</label>
-            <input value=" {{ $address->city }} " name="city" id="city" type="text" tabindex="1" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="country">Pays</label>
-            <input value=" {{ $address->country }} " name="country" id="country" type="text" tabindex="1" required autofocus>
-          </div>
-          <button class="btn btn-primary" name="submit" type="submit" id="contact-submit">Modifier l'adresse</button>
-        </form>
+  <div class="editaccountcontent">
+    <div class="col-8">
+      <form action="{{ route('Address.update', ['Address'=>$address->id]) }}" method="POST" >
+        @csrf
+        @method('PUT')
+        <div class="form-group editinput">
+          <label for="address">Adresse</label>
+          <input value=" {{ $address->address }} " name="address" id="address" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="form-group editinput">
+          <label for="zip_code">Cope postal</label>
+          <input value=" {{ $address->zip_code }} " name="zip_code" id="zip_code" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="form-group editinput">
+          <label for="city">Ville</label>
+          <input value=" {{ $address->city }} " name="city" id="city" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="form-group editinput">
+          <label for="country">Pays</label>
+          <input value=" {{ $address->country }} " name="country" id="country" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="buttonModify">
+          <button class="editbutton" name="submit" type="submit" id="contact-submit">Modifier l'addresse</button>
+        </div>
+      </form>
+      <div class="buttonModify">
         <form action="{{ route('Address.destroy', ['Address' => $address->id]) }}" method="POST" style="display: contents">
           @csrf
           @method('DELETE')
-          <button class="btn btn-danger" type="submit">
+          <button class="editbutton" type="submit">
             <span class="fa fa-trash">Supprimer l'addresse</span>
           </button>
         </form>
       </div>
     </div>
-  @endguest
+  </div>
 </div>
 @endsection
