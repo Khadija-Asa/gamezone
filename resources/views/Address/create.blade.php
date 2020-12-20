823aac7abafaff3b779b9f59926453b08827ebb1
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-12-20 13:39:26 +0100

    added style to address create

--- resources/views/Address/create.blade.php
+++ resources/views/Address/create.blade.php
@@ -2,40 +2,31 @@
 
 @section('content')
 <div class="container">
-  @guest
-    <p>Vous n'avez pas la permission d'accéder à cette page</p>
-  @else
-    <div class="row">
-      <div class="col-8">
-        <form action="{{ route('Address.store') }}" method="POST" >
-          @csrf
-          <div class="form-group">
-            <label for="address">Adresse</label>
-            <input name="address" id="address" type="text" tabindex="1" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="zip_code">Code Postal</label>
-            <input name="zip_code" id="zip_code" type="text" tabindex="1" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="city">Ville</label>
-            <input name="city" id="city" type="text" tabindex="1" required autofocus>
-          </div>
-          <div class="form-group">
-            <label for="country">Pays</label>
-            <input name="country" id="country" type="text" tabindex="1" required autofocus>
-          </div>
-          <button class="btn btn-primary" name="submit" type="submit" id="contact-submit">Ajouter une adresse</button>
-        </form>
-        {{-- <form action="{{ route('User.destroy', ['User' => $user->id]) }}" method="POST" style="display: contents">
-          @csrf
-          @method('DELETE')
-          <button class="btn btn-danger" type="submit">
-            <span class="fa fa-trash">Supprimer le compte</span>
-          </button>
-        </form> --}}
-      </div>
+  <div class="editaccountcontent">
+    <div class="col-8">
+      <form action="{{ route('Address.store') }}" method="POST" >
+        @csrf
+        <div class="form-group editinput">
+          <label for="address">Adresse</label>
+          <input name="address" id="address" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="form-group editinput">
+          <label for="zip_code">Code Postal</label>
+          <input name="zip_code" id="zip_code" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="form-group editinput">
+          <label for="city">Ville</label>
+          <input name="city" id="city" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="form-group editinput">
+          <label for="country">Pays</label>
+          <input name="country" id="country" type="text" tabindex="1" required autofocus>
+        </div>
+        <div class="buttonModify">
+          <button class="editbutton" name="submit" type="submit" id="contact-submit">Ajouter une addresse</button>
+        </div>
+      </form>
     </div>
-  @endguest
+  </div>
 </div>
 @endsection
